78a7f62b3aa578747c8563dd2d851faba908c56b
dennis-codes <dennis@ngeni.io>
2026-02-11 14:35:20 +0300

    change middleware.ts -> proxy.ts

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  // Protect all pages except auth routes, next internals, and static assets
-  matcher: ["/profile/:path*"],
-};
