f15b3e8a2d50d2f2c49cf04f9193da0c8cfadc72
dennis-codes <dennis@ngeni.io>
2026-02-11 14:40:49 +0300

    change middleware.ts -> proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,23 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth();
+export default withAuth(function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/api/proxy")) {
+    const url = req.nextUrl.clone();
+    url.hostname = "api.yourbackend.com";
+    url.protocol = "https";
+
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ["/profile/:path*"],
+  matcher: [
+    "/profile/:path*",
+    "/api/proxy/:path*",
+  ],
 };
